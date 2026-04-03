9b45c1505374e96ac4df89964143c2499bb63cd4
Theline <1297618009@qq.com>
2026-04-03 16:51:50 +0800

    agent创建

---
+++ Taobao.Agent/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http.Json;
+Console.WriteLine("淘宝数据采集已启动");
+var apiUrl = "https://localhost:7177/api/Orders/sync";
+var mockOrders = new[]
+{
+    new
+    {
+        orderId = "Agent_" + Guid.NewGuid().ToString().Substring(0,8),
+        title = "测试订单",
+        price = 499m,
+        buyTime = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss"),
+    }
+};
